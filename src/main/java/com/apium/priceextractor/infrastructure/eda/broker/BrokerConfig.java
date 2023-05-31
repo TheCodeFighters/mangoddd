f706a445ddc0cfa3c39c259f6c38b70a3a831be8
Oscar Lopez <oscarll@ext.inditex.com>
2023-05-31 05:07:56 +0200

    [feature/hexagonal-architecture-for-repo] get price without discount

--- src/main/java/com/apium/priceextractor/infrastructure/eda/broker/BrokerConfig.java
+++ src/main/java/com/apium/priceextractor/infrastructure/eda/broker/BrokerConfig.java
@@ -1,35 +1,22 @@
 package com.apium.priceextractor.infrastructure.eda.broker;
 
-import com.apium.priceextractor.domain.event.DomainEvent;
 import com.apium.priceextractor.infrastructure.eda.broker.NoOpBroker.NoopBroker;
 import com.apium.priceextractor.infrastructure.eda.broker.kafka.KafkaMessageBroker;
-import org.apache.kafka.common.serialization.StringSerializer;
-import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 
 @Configuration
 public class BrokerConfig {
 
+  //  @Bean()
+  //  public KafkaMessageBroker createKafkaBroker(KafkaTemplate<String, String> kafkaTemplate) {
+  //    return new KafkaMessageBroker(kafkaTemplate);
+  //  }
+
   @Bean()
   @Primary
-  public KafkaMessageBroker createKafkaBroker(KafkaTemplate<String, String> kafkaTemplate) {
-    //    DefaultKafkaProducerFactory<String, String> producerFactory =
-    //        new DefaultKafkaProducerFactory<>(
-    //            kafkaProperties.buildProducerProperties(),
-    //            new StringSerializer(),
-    //            new StringSerializer()
-    //        );
-    //
-    //    KafkaTemplate<String, String> kafkaTemplate = new KafkaTemplate<>(producerFactory);
-    return new KafkaMessageBroker(kafkaTemplate);
-  }
-
-  @Bean()
-
   public NoopBroker createNoOpKafkaBroker() {
     return new NoopBroker();
   }
