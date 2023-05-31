386aacf5bf79dc465b1afb23b1b7caafe2636667
Oscar Lopez <oscarll@ext.inditex.com>
2023-05-31 05:58:16 +0200

    [feature/hexagonal-architecture-for-repo] get price without discount

--- src/main/java/com/apium/priceextractor/application/priceDiscount/ProductPriceDiscountService.java
+++ src/main/java/com/apium/priceextractor/application/priceDiscount/ProductPriceDiscountService.java
@@ -1,15 +1,10 @@
 package com.apium.priceextractor.application.priceDiscount;
 
-import com.apium.priceextractor.domain.Date;
 import com.apium.priceextractor.domain.DiscountPercentage;
-import com.apium.priceextractor.domain.DomainEventPublisher;
 import com.apium.priceextractor.domain.ProductDiscountAgg;
 import com.apium.priceextractor.domain.ProductDiscountId;
 import com.apium.priceextractor.domain.ProductDiscountRepository;
 import com.apium.priceextractor.domain.ProductId;
-import com.apium.priceextractor.domain.event.EventId;
-import com.apium.priceextractor.domain.event.ProductDiscountAggWasCreatedEvent;
-import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -17,15 +12,12 @@ public class ProductPriceDiscountService {
 
   private final ProductDiscountRepository productPriceDiscountRepository;
 
-  private final DomainEventPublisher domainEventPublisher;
 
   public ProductPriceDiscountService(
-      ProductDiscountRepository productPriceDiscountRepository, DomainEventPublisher domainEventPublisher) {
+      ProductDiscountRepository productPriceDiscountRepository) {
     this.productPriceDiscountRepository = productPriceDiscountRepository;
-    this.domainEventPublisher = domainEventPublisher;
   }
 
-  @Transactional
   public void createProductPriceDiscount(CreateProductDiscountRequestDto requestDto) {
     ProductDiscountAgg productDiscountAgg = new ProductDiscountAgg(
         ProductDiscountId.fromString(requestDto.productId()),
@@ -33,9 +25,6 @@ public class ProductPriceDiscountService {
         DiscountPercentage.fromDouble(requestDto.discountPercentage())
     );
     productPriceDiscountRepository.save(productDiscountAgg);
-    domainEventPublisher.publish(
-        new ProductDiscountAggWasCreatedEvent(EventId.create(), Date.Now(), productDiscountAgg.toDto())
-    );
   }
 
 }
--- src/main/java/com/apium/priceextractor/domain/DomainEventPublisher.java
+++
@@ -1,9 +0,0 @@
-package com.apium.priceextractor.domain;
-
-import com.apium.priceextractor.domain.event.DomainEvent;
-import com.fasterxml.jackson.core.JsonProcessingException;
-
-public interface DomainEventPublisher {
-
-  void publish(DomainEvent event);
-}
