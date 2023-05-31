32b3a08cfc4edac14f7d5cd2fb6b5f9b5bbbf586
Oscar Lopez <oscarll@ext.inditex.com>
2023-05-31 18:23:07 +0200

    [feature/hexagonal-architecture-for-repo] get price without discount

--- src/main/java/com/apium/priceextractor/domain/DomainEventPublisher.java
+++ src/main/java/com/apium/priceextractor/domain/DomainEventPublisher.java
@@ -1,7 +1,6 @@
 package com.apium.priceextractor.domain;
 
 import com.apium.priceextractor.domain.event.DomainEvent;
-import com.fasterxml.jackson.core.JsonProcessingException;
 
 public interface DomainEventPublisher {
 
